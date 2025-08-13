58968237eb6e173e1c29cfb89c27b3a98cd92caa
l2yuPa <jeungwon28@gmail.com>
2025-08-13 14:24:19 +0900

    feat(shared): AbstractTime

---
+++ src/main/java/online/lifeasgame/shared/entity/AbstractTime.java
@@ -0,0 +1,23 @@
+package online.lifeasgame.shared.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AbstractTime {
+    @CreatedBy
+    @Column(name = "created_at", nullable = false, updatable = false)
+    protected Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    protected Instant updatedAt;
+}
