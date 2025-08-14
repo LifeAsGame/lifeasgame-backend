2b71cbb8214b696e38d7eda6c5710b02ab8f8ee6
l2yuPa <jeungwon28@gmail.com>
2025-08-14 19:55:58 +0900

    fix(global): Missing @EnableAsync Setting

--- src/main/java/online/lifeasgame/global/config/AsyncExecutorConfig.java
+++ src/main/java/online/lifeasgame/global/config/AsyncExecutorConfig.java
@@ -1,10 +1,13 @@
 package online.lifeasgame.global.config;
 
+import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskDecorator;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+@EnableAsync
 @Configuration
 public class AsyncExecutorConfig {
 
@@ -16,6 +19,9 @@ public class AsyncExecutorConfig {
         executor.setQueueCapacity(100);
         executor.setThreadNamePrefix("async-");
         executor.setTaskDecorator(decorator);
+        executor.setRejectedExecutionHandler(new CallerRunsPolicy());
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(30);
         executor.initialize();
         return executor;
     }
