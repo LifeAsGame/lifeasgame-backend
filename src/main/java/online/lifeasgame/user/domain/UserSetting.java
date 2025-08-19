c95ee90bd0f12f77d32a0f4cc89a45588c7dcde2
l2yuPa <jeungwon28@gmail.com>
2025-08-19 15:02:12 +0900

    refactor(user): make UserSetting to reference on User

--- src/main/java/online/lifeasgame/user/domain/UserSetting.java
+++ src/main/java/online/lifeasgame/user/domain/UserSetting.java
@@ -2,7 +2,11 @@ package online.lifeasgame.user.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -14,8 +18,13 @@ import online.lifeasgame.shared.entity.AbstractTime;
 public class UserSetting extends AbstractTime {
 
     @Id
-    @Column(name = "user_id")
+    @Column(name="user_id")
     private Long userId;
+
+    @MapsId
+    @OneToOne(fetch= FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private User user;
 
     @Column(length=10)
     private String language = "ko";
