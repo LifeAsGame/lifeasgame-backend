a65eefaa7e0753c06324a6b84153a4e8b0b583f0
l2yuPa <jeungwon28@gmail.com>
2025-08-23 01:46:18 +0900

    feat(character): PlayerAchievement

--- src/main/java/online/lifeasgame/character/domain/Achievement.java
+++ src/main/java/online/lifeasgame/character/domain/Achievement.java
@@ -7,22 +7,25 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
+import online.lifeasgame.shared.annotation.AggregateRoot;
 import online.lifeasgame.shared.entity.AbstractTime;
 
 @Entity
+@AggregateRoot
 @Table(name="achievements")
 public class Achievement extends AbstractTime {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length=60, unique=true)
+    @Column(length = 60, unique = true)
     private String code;
 
-    @Column(length=60, nullable=false)
+    @Column(length = 60, nullable = false)
     private String name;
 
     @Lob
-    @Column(name="desc_md")
+    @Column(name = "desc_md")
     private String descMd;
 }
