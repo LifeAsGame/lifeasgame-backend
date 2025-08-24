0a6abbb5a2caa4e863de47f473747a03f280e7f4
l2yuPa <jeungwon28@gmail.com>
2025-08-24 11:21:49 +0900

    feat(skill): PlayerSkill, PlayerSkillPoint

---
+++ src/main/java/online/lifeasgame/skill/domain/PlayerSkill.java
@@ -0,0 +1,36 @@
+package online.lifeasgame.skill.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.Version;
+import online.lifeasgame.shared.annotation.AggregateRoot;
+
+@Entity
+@AggregateRoot
+@Table(
+        name = "player_skills",
+        uniqueConstraints = @UniqueConstraint(name = "pk_player_skill", columnNames = {"player_id", "skill_id"})
+)
+public class PlayerSkill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "player_id", nullable = false)
+    private Long playerId;
+
+    @Column(name = "skill_id", nullable = false)
+    private Long skillId;
+
+    @Column(nullable = false)
+    private int level = 0;
+
+    @Version
+    private Long version;
+}
