24ed3e4995c2a120c1cf8b5f21a01178bd3eebd2
l2yuPa <jeungwon28@gmail.com>
2025-09-03 13:42:11 +0900

    feat(core, platform): CurrentUserAccessor

---
+++ src/main/java/online/lifeasgame/platform/security/FixedDevCurrentUserAccessor.java
@@ -0,0 +1,18 @@
+package online.lifeasgame.platform.security;
+
+import java.util.Optional;
+import online.lifeasgame.core.security.CurrentUserAccessor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Primary
+@Component
+@Profile({"local", "dev", "test"})
+public class FixedDevCurrentUserAccessor implements CurrentUserAccessor {
+
+    @Override
+    public Optional<Long> currentUserId() {
+        return Optional.of(1L);
+    }
+}
