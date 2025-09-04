7b7c7895a6d3c05eefe235aa21230ba9ec5f40d5
l2yuPa <jeungwon28@gmail.com>
2025-09-05 08:43:28 +0900

    feat(character): PlayerError value

--- src/main/java/online/lifeasgame/character/application/PlayerReader.java
+++ src/main/java/online/lifeasgame/character/application/PlayerReader.java
@@ -2,7 +2,9 @@ package online.lifeasgame.character.application;
 
 import lombok.RequiredArgsConstructor;
 import online.lifeasgame.character.domain.Player;
+import online.lifeasgame.character.domain.error.PlayerError;
 import online.lifeasgame.character.domain.repository.PlayerRepository;
+import online.lifeasgame.core.error.DomainException;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +14,8 @@ public class PlayerReader {
     private final PlayerRepository playerRepository;
 
     public Player getPlayerInfo(Long playerId) {
-        return playerRepository.findById(playerId).orElseThrow();
+        return playerRepository.findById(playerId).orElseThrow(
+                () -> new DomainException(PlayerError.PLAYER_NOT_FOUND)
+        );
     }
 }
