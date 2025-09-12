870893d07c08dcd648e90572a9993acc41f605ed
l2yuPa <jeungwon28@gmail.com>
2025-09-12 17:55:22 +0900

    feat(character): validation on AdminTitleRequest

--- src/main/java/online/lifeasgame/character/presentation/request/AdminTitleRequest.java
+++ src/main/java/online/lifeasgame/character/presentation/request/AdminTitleRequest.java
@@ -1,15 +1,17 @@
 package online.lifeasgame.character.presentation.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class AdminTitleRequest {
 
     private AdminTitleRequest() {
     }
 
     public record CreateTitle(
-            String code,
-            String name,
-            String category,
-            String descMd
+            @NotBlank String code,
+            @NotBlank String name,
+            @NotBlank String category,
+            @NotBlank String descMd
     ) {
         public static CreateTitle of(String code, String name, String category, String descMd) {
             return new CreateTitle(code, name, category, descMd);
