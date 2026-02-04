5a67ac66332055a18a946b4dc761b74ea23e3d7f
l2yuPa <jeungwon28@gmail.com>
2026-02-04 22:17:54 +0900

    feat(character): revoke hobby admin api

--- src/main/java/online/lifeasgame/character/api/admin/spec/AdminPlayerAchievementApiSpecV1.java
+++ src/main/java/online/lifeasgame/character/api/admin/spec/AdminPlayerAchievementApiSpecV1.java
@@ -1,6 +1,7 @@
 package online.lifeasgame.character.api.admin.spec;
 
 import io.swagger.v3.oas.annotations.Operation;
+import online.lifeasgame.character.api.admin.response.AdminPlayerAchievementResponse;
 import online.lifeasgame.character.api.admin.response.AdminPlayerAchievementResponse.Granted;
 import online.lifeasgame.core.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,12 @@ public interface AdminPlayerAchievementApiSpecV1 {
 
     @Operation(summary = "Player Achievement 지급", description = "Player에게 Achievement를 지급합니다")
     ResponseEntity<ApiResponse<Granted>> grantAchievement(
+            @PathVariable Long playerId,
+            @PathVariable Long achievementId
+    );
+
+    @Operation(summary = "Player Achievement 회수", description = "Player의 Achievement를 회수합니다.")
+    ResponseEntity<ApiResponse<AdminPlayerAchievementResponse.Revoked>> revokeAchievement(
             @PathVariable Long playerId,
             @PathVariable Long achievementId
     );
--- src/main/java/online/lifeasgame/character/api/admin/spec/draft/AdminPlayerAchievementApiDraftSpecV1.java
+++ src/main/java/online/lifeasgame/character/api/admin/spec/draft/AdminPlayerAchievementApiDraftSpecV1.java
@@ -1,16 +1,4 @@
 package online.lifeasgame.character.api.admin.spec.draft;
 
-import io.swagger.v3.oas.annotations.Operation;
-import online.lifeasgame.character.api.admin.response.AdminPlayerAchievementResponse;
-import online.lifeasgame.core.response.ApiResponse;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-
 public interface AdminPlayerAchievementApiDraftSpecV1 {
-
-    @Operation(summary = "Player Achievement 회수", description = "Player의 Achievement를 회수합니다.")
-    ResponseEntity<ApiResponse<AdminPlayerAchievementResponse.Revoked>> revokeAchievement(
-            @PathVariable Long playerId,
-            @PathVariable Long achievementId
-    );
 }
